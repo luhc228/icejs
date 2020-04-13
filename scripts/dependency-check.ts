a24614ee8623720d5a3357eb7bae7049e8ff0a56
思忠 <chenbinbin.cb@alibaba-inc.com>
2020-04-13 20:58:10 +0800

    chore: fix lint

--- scripts/dependency-check.ts
+++ scripts/dependency-check.ts
@@ -1,20 +1,18 @@
-import * as execa from 'execa'
-import { join } from 'path';
+import * as execa from 'execa';
 import getPackages from './fn/getPackages';
 
+// eslint-disable-next-line
 const chalk = require('chalk');
 
 (async () => {
   const { packageDirs } = await getPackages();
   packageDirs.forEach((pkgDir) => {
-    const pkgContent = require(join(pkgDir, 'package.json'))
-    const { name } = pkgContent;
     execa.commandSync(`dependency-check ${pkgDir} --missing`, {
       cwd: pkgDir,
       stdio: 'inherit'
     });
   });
 })().catch((e) => {
-  console.log(chalk.red(`\n ⚠️  ⚠️  ⚠️  依赖检查失败\n\n`), e)
-  process.exit(128)
+  console.log(chalk.red(`\n ⚠️  ⚠️  ⚠️  依赖检查失败\n\n`), e);
+  process.exit(128);
 });
